4508466e66fe2919e70920d23334c4828f11d7ad
Allen Yi <liurq93@163.com>
2020-04-03 15:41:08 +0800

    Create deploy.sh

---
+++ shell/deploy.sh
@@ -0,0 +1,3 @@
+echo "[deploy] start deploy circleci-demo"
+ps -ef | grep springboot-demo1 | grep -v grep | awk '{print $2}' | xargs kill -9  || echo "kill fail "
+java -jar target/springboot-demo1-0.0.1-SNAPSHOT.jar > a.log &
